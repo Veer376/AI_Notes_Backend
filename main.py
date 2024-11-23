c7ebfd54a6be762eef00e9268538d4f6d5f05cec
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-11-23 12:17:48 +0530

    trying with the railway continunous server

--- main.py
+++ main.py
@@ -31,6 +31,4 @@ async def root():
 
 # @/calculate
 app.include_router(calculator_router)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", PORT))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
