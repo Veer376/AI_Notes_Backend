84868c9608d8e81544de3d9f7b5fa185c7e426ac
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-11-23 14:43:30 +0530

    render this time following the proper documentation

--- main.py
+++ main.py
@@ -31,7 +31,5 @@ async def root():
 
 # @/calculate
 app.include_router(calculator_router)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Railway provides a PORT env variable
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
